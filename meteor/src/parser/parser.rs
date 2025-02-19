7d9fcc42118ecec63ca1fee3516e1923ec23b46a
Kishan S Patel <bishan.batel@protonmail.com>
2025-02-19 12:12:14 -0800

    Added unless

--- meteor/src/parser/parser.rs
+++ meteor/src/parser/parser.rs
@@ -316,16 +316,30 @@ where
         let inline_expr = assignment.labelled("expression").as_context();
 
         let while_expr = just(Token::While)
-            .ignore_then(expr.clone())
+            .map(|_| false)
+            .or(just(Token::Unless).map(|_| true))
+            .then(expr.clone())
             .then(block.clone())
-            .map_with(|(cond, body), e| {
-                (
+            .map_with(|((unless, cond), body), e| {
+                let while_expr = (
                     Expression::While {
                         condition: Box::new(cond),
                         then: Box::new(body),
                     },
                     e.span(),
-                )
+                );
+
+                if unless {
+                    (
+                        Expression::UnaryOp {
+                            operator: Operator::Not,
+                            rhs: Box::new(while_expr),
+                        },
+                        e.span(),
+                    )
+                } else {
+                    while_expr
+                }
             })
             .boxed();
 
